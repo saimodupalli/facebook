e643187065edceb7e9322b587e864eed35400110
saikk <ram@gmail.com>
2018-04-09 11:27:02 +0530

    new project

---
+++ chinnaco/src/main/java/com/chinnaco/App.java
@@ -0,0 +1,13 @@
+package com.chinnaco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
